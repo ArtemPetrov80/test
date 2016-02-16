09ce031d9ab99811be9ca256e89c938edc52085b
Artem <artem.petrov80@gmail.com>
2016-02-17 00:42:57 +0600

    lock

--- main.cpp
+++ main.cpp
@@ -14,8 +14,12 @@
 #include <string.h>
 
 
-#define PATH "/home/box/main.pid"
+#define PATH "/home/artem/main.pid"
 #define MAX_PID_LEN 10
+
+pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+pthread_spinlock_t spinlock;
+pthread_rwlock_t rw_lock = PTHREAD_RWLOCK_INITIALIZER;
 
 void createFileWithPid(int pid, const char * path)
 {
@@ -36,20 +40,64 @@ printf("pid = %d\n", pid);
 
 }
 
-void * thread_body(void *arg)
+void * thread_mutex(void *arg)
 {
-	for (int i = 0; i < 10; ++i)
-		std::cout << "Thread 2 : " << i << std::endl;
+	for (int i = 0; i < 10; )
+	{
+		pthread_mutex_lock(&mtx);
+		++i;
+		pthread_mutex_unlock(&mtx);
+		std::cout << "Thread mut : " << i << std::endl;
+	}
 }
 
+void * thread_spin(void *arg)
+{
+        for (int i = 0; i < 10; ++i)
+	{
+		pthread_spin_lock(&spinlock);
+		++i;
+		pthread_spin_unlock(&spinlock);
+		std::cout << "Thread spin : " << i << std::endl;
+	}
+}
+
+void * thread_rd(void *arg)
+{
+        for (int i = 0; i < 10; ++i)
+	{
+		pthread_rwlock_rdlock(&rw_lock);
+		++i;
+		pthread_rwlock_rdlock(&rw_lock);
+                std::cout << "Thread r_lock : " << i << std::endl;
+	}
+}
+
+void * thread_wr(void *arg)
+{
+        for (int i = 0; i < 10; ++i)
+        {
+                pthread_rwlock_wrlock(&rw_lock);
+                ++i;
+                pthread_rwlock_wrlock(&rw_lock);
+                std::cout << "Thread r_lock : " << i << std::endl;
+        }
+}
 
 int main()
 {
     int pid = getpid();
     createFileWithPid(pid, PATH);
 
-    pthread_t  p1, p2;
-    (void) pthread_create(&p1, NULL, thread_body, NULL);
+    pthread_t  p1, p2, p3, p4;
+    (void) pthread_create(&p1, NULL, thread_mutex, NULL);
+
+    pthread_spin_init(&spinlock, PTHREAD_PROCESS_PRIVATE);
+    (void) pthread_create(&p2, NULL, thread_spin, NULL);
+
+    (void) pthread_create(&p3, NULL, thread_rd, NULL);
+
+    (void) pthread_create(&p4, NULL, thread_wr, NULL);
 
     for(int i =0; i < 100; ++i)
     {
