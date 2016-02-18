8079493fa96f18705c95ba6bc8def7bb197eb475
Artem <artem.petrov80@gmail.com>
2016-02-18 09:24:07 +0600

    posix sem

---
+++ posix_sem.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <semaphore.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main()
+{
+    sem_t * sem_id;
+    sem_id = sem_open("/test.sem", O_CREAT, 0600, 0);
+    if(sem_id == SEM_FAILED) {
+        perror("sem_open");
+        return;
+    }
+    return 0;
+}
